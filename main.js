045a77c05ee07dae19edbdedd9b929e1996dfbb0
marcos gaspari <marcosgaspari098@hotmail.com>
2021-11-03 18:55:59 -0300

    atv4

--- main.js
+++ main.js
@@ -38,6 +38,8 @@ console.log(addedApps.join('\n'));
 
 // (Exercício 4) Identifique quais funções são high-order
     // Adicione um comentário identificando-as
+    // filter(), 
+    // map();
 
 // (Exercício 5) Crie um novo arquivo main2.js (baseado no main.js) que seleciona todos as apps adicionadas
 // depois do ano 2000 e atualizadas em 2020. Ao imprimir as apps, além do nome, imprima
